825f2bf20a19dc4f216d2b725872905b84f25c0e
GiuliBelt <giulianabeltran21@gmail.com>
2026-02-20 01:11:41 -0300

    Create VOPostreListado.java

---
+++ src/logica/vo/VOPostreListado.java
@@ -0,0 +1,28 @@
+package logica.vo;
+
+public class VOPostreListado {
+    private String codigo;
+    private String nombre;
+    private float precio;
+    private String tipo;
+}
+
+public VOVenta(){ 
+
+}
+
+public String getCodigo(){
+  return codigo;
+}
+
+public String getNombre(){
+  return nombre;
+}
+
+public float getPrecio(){
+  return precio;
+}
+
+public String getTipo(){
+  return tipo;
+}
