2c7c7cb5c050458a70b96f63faa226d333105b91
GiuliBelt <giulianabeltran21@gmail.com>
2026-02-20 01:24:59 -0300

    Create VOPostreAlta.java

---
+++ src/logica/vo/VOPostreAlta.java
@@ -0,0 +1,38 @@
+package logica.vo;
+
+private String Codigo;
+private String Nombre;
+private float Precio;
+private String Tipo;
+private String TipoEndulzante;
+private String Descripcion;
+
+
+public VOPostreAlta(){
+
+}
+
+public String getCodigo() {
+        return codigo;
+}
+
+public String getNombre() {
+        return nombre;
+}
+
+public float getPrecio() {
+        return precio;
+}
+
+public String getTipo() {
+        return tipo;
+}
+
+public String getTipoEndulzante() {
+        return tipoEndulzante;
+}
+
+public String getDescripcion() {
+        return descripcion;
+}
+}
