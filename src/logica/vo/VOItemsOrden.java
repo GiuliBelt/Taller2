f6ea4f4a5bd36ab6341d7512397e795b5c7bab65
GiuliBelt <giulianabeltran21@gmail.com>
2026-02-20 01:34:46 -0300

    Create VOItemsOrden.java

---
+++ src/logica/vo/VOItemsOrden.java
@@ -0,0 +1,27 @@
+package logica.vo;
+
+private int NumVenta;
+private String Codigo;
+private float Precio;
+private String Tipo;
+private int CantUnidades;
+
+public class VOItemsOrden {
+
+}
+
+public int getNumVenta() {
+      return numVenta;
+}
+
+public String getCodigo() {
+      return codigo;
+}
+
+public float getPrecio() {
+      return precio;
+    }
+
+public String getTipo() {
+      return tipo;
+}
